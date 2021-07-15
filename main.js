73e72e38ca5f4221de277eabe7199995985f8817
oc <oskar.cejman@nxo.pl>
2021-07-15 13:50:06 +0200

    Dodanie menu z lewej strony

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
-let myRaper = angular.module('myRaper',[]);
+let myRaper = angular.module('myRaper',['ngRoute']);
+
+myRaper.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+        .when('/mazowieckie',{
+            templateUrl:'views/mazowieckie.html',
+            controller:'raperController'
+        })
+        .when('/srem',{
+            templateUrl:'cities/srem.html',
+            controller:'raperController'
+        })
+        .otherwise({
+            rediretTo:'/'
+        })
+}])
 
 myRaper.controller('raperController', ['$scope', function($scope){
 $scope.dupa = 'test';
